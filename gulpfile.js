a47a1792607f13c8aa326bfdbee812ba028ae37b
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-10-02 14:01:41 +0200

    chore: rename package

--- gulpfile.js
+++ gulpfile.js
@@ -96,7 +96,7 @@ gulp.task('npm', function() {
   var targetPkgJson = {};
   var fieldsToCopy = ['version', 'description', 'keywords', 'author', 'repository', 'license', 'bugs', 'homepage'];
 
-  targetPkgJson['name'] = '@ng-bootstrap/ng-bootstrap';
+  targetPkgJson['name'] = '@mattlewis92/ng-bootstrap';
 
   fieldsToCopy.forEach(function(field) { targetPkgJson[field] = pkgJson[field]; });
 
